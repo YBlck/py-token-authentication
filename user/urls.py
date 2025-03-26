ec48d757e374fb74a5b46044d47c681372a879bd
Yura Chornyi <chorny.yura@gmail.com>
2025-03-26 17:47:32 +0200

    feat: add login endpoint for user to obtain token

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from user.views import UserCreateView
 
@@ -6,4 +7,5 @@ app_name = "user"
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="register"),
+    path("login/", ObtainAuthToken.as_view(), name="login"),
 ]
