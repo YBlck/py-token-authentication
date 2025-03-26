c31f1efe0ad1858bab7fde12440629254c2dd247
Yura Chornyi <chorny.yura@gmail.com>
2025-03-26 17:19:47 +0200

    feat: add user create view

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,9 @@
-# write your code here
+from django.urls import path
+
+from user.views import UserCreateView
+
+app_name = "user"
+
+urlpatterns = [
+    path("register/", UserCreateView.as_view(), name="register"),
+]
