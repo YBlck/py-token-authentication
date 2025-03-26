d6d54f1696da93ca625780c814c230fb3993ff38
Yura Chornyi <chorny.yura@gmail.com>
2025-03-26 18:56:16 +0200

    fix: fix login path

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from rest_framework.authtoken.views import ObtainAuthToken
 
-from user.views import UserCreateView, ManageUserView
+from user.views import UserCreateView, ManageUserView, ObtainTokenView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="create"),
-    path("login/", ObtainAuthToken.as_view(), name="login"),
+    path("login/", ObtainTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
