82c0874e2a2751187964277abdf37cb233d4139a
Yura Chornyi <chorny.yura@gmail.com>
2025-03-26 18:00:25 +0200

    feat: add me endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.authtoken.views import ObtainAuthToken
 
-from user.views import UserCreateView
+from user.views import UserCreateView, ManageUserView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="register"),
     path("login/", ObtainAuthToken.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
